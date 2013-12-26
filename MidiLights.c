38d0c631746da13e7f5c4ace74474904b4ca666e
akashi <akashi@18c69580-d92f-d848-b4a5-6284aa11c943>
2013-12-26 14:17:04 +0000

    MIDI OUT(鍵盤)の取り込みに対応。
    git-svn-id: svn://localhost/src/MidiLights@423 18c69580-d92f-d848-b4a5-6284aa11c943

--- MidiLights.c
+++ MidiLights.c
@@ -234,17 +234,138 @@ retry:
 	}
 }
 
-volatile int gFlag;
+// USART1, Rx Complete
+ISR(USART1_RX_vect)
+{
+	static uint8_t	operand = 0;
+	static uint8_t  operand_bak = 0;
+	static int8_t	note = -1;
+	static uint8_t	ignore_count = 0;
+
+	if (ignore_count) {
+		ignore_count--;
+		return;
+	}
+
+	uint8_t d = UDR1;
+
+//	gData[idx] = UDR1;
+//	idx++;
+//	idx &= 0x07;
+
+	retry:
+	switch (operand) {
+		case 0:	// 未定
+		switch (d & 0xF0) {
+			case 0x90:	// ノートオン
+			case 0x80:	// ノートオフ
+			note = -1;
+			// break しない
+			case 0xB0:	// コントロールチェンジ or モード・チェンジ
+			case 0xC0:	// プログラムチェンジ
+			operand_bak = operand = 0xF0 & d;
+			break;
+			case 0xF0:	// システム・リアルタイム・メッセージ
+			switch (d) {
+				case 0xF0:
+				operand = d;
+				break;
+			}
+			break;
+
+			default:	// ランニングステータス
+			switch (operand_bak) {
+				case 0x90:
+				case 0x80:
+				if ((d & 0x80) == 0x00) {
+					operand = operand_bak;
+					goto retry;
+				}
+				break;
+			}
+		}
+		break;
+		case 0x90:	// ノートオン
+		case 0x80:	// ノートオフ
+		// データバイトにて指定するノートナンバーとは、
+		// 最も低い音を0、最も高い音を127と割り当てた音の高さのことである。
+		// 中央ハにはノートナンバー60が割り当てられ、
+		// 88鍵盤のグランドピアノで出せる音域は
+		// ノートナンバー21～108と割り当てられる
+
+		if (note == -1) {
+			if (d < 21 || 108 < d) {
+				ignore_count = 1;
+				operand = 0;
+				break;
+			}
+			note = d - 21;
+			break;
+		}
+
+		if (d == 0 || d > 127) {
+			operand = 0x80;
+		}
+
+		uint8_t idx = note / 11;
+		uint16_t data = 1 << (note % 11);
+		switch (operand) {
+			case 0x90:	// ノートオン
+			gData[idx] |= data;
+			break;
+			case 0x80:	// ノートオフ
+			gData[idx] &= ~data;
+			break;
+		}
+		note = -1;
+		operand = 0;
+		break;
+		case 0xB0:	// コントロールチェンジ or モード・チェンジ
+		// オール・ノート・オフ
+		switch (d) {
+			case 0x78:
+			case 0x79:
+			case 0x7B:
+			case 0x7C:
+			case 0x7D:
+			case 0x7E:
+			case 0x7F:
+			for (int i = 0; i < 8; i++) {
+				gData[i] = 0;
+			}
+			break;
+		}
+		// break しない
+		case 0xC0:	// プログラムチェンジ
+		ignore_count = 1;
+		operand = 0;
+		break;
+		case 0xF0:	// システム・リアルタイム・メッセージ
+		// 終了メッセージ
+		if (d == 0xF7) {
+			operand = 0;
+		}
+		break;
+	}
+}
 
 // USART0, Tx Complete 
 ISR(USART0_TX_vect)
 {
-	gData[0] = 0x1;
-	gFlag = 0;
 }
 
 // USART0 Data Register Empty 
 ISR(USART0_UDRE_vect)
+{
+}
+
+// USART1, Tx Complete
+ISR(USART1_TX_vect)
+{
+}
+
+// USART1 Data Register Empty
+ISR(USART1_UDRE_vect)
 {
 }
 
@@ -307,17 +428,19 @@ int main(void)
 
 	// D0は、MIDI入力。D1は、MIDI出力。D3は、赤外線リモコン受信用外部割り込み
 	// D5-D7は、LEDの制御スイッチ
-	DDRD = 0xF6;
-	PORTD = 0xEB;
+	DDRD = 0xF2;
+	PORTD = 0xEF;
 
 	// タイマ設定
 	TCCR0B = 0x01;	// プリスケーラは、1
 	TIMSK0 = 0x01;	// タイマ２オーバーフロー割り込み許可
 	
 	UBRR0 = 19;		// MIDIのボーレートは、31.25Kbps  UBRRn = (fosc / (16 * BAUD)) - 1; for 10MHz
+	UBRR1 = 19;		// MIDIのボーレートは、31.25Kbps  UBRRn = (fosc / (16 * BAUD)) - 1; for 10MHz
 //	UBRR0 = 15;		// MIDIのボーレートは、31.25Kbps  UBRRn = (fosc / (16 * BAUD)) - 1; for 8MHz
 //	UCSR0B = 0xB8;	// 送受信および受信完了送信空き割り込み許可
 	UCSR0B = 0x98;	// 送受信および受信完了割り込み許可
+	UCSR1B = 0x90;	// 受信および受信完了割り込み許可
 
 	MMC_hw_init();
 	spi_init();
